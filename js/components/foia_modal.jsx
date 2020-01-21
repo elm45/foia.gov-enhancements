a1d720d0d2aeb3aebc585159f0b5ce8cb0c02173
Liz Mackie <elizabeth.mackie@acquia.com>
2020-01-21 15:52:39 -0500

    FOIA-270: Adding modal props.

--- js/components/foia_modal.jsx
+++ js/components/foia_modal.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import PropTypes from 'prop-types';
 
 Modal.setAppElement('#main');
 
@@ -26,49 +27,22 @@ class FoiaModal extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.openModal} className="usa-button usa-button-outline" type="button">Open Modal</button>
+        <button onClick={this.openModal} className="usa-button usa-button-outline usa-button-small" type="button">{this.props.triggerText}</button>
         <Modal
           isOpen={this.state.modalIsOpen}
-          // onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
-          aria={{
-            labelledby: 'heading_id_placehoder',
-            describedby: 'full_description',
-          }}
-          contentLabel="Example Modal"
+          contentLabel={this.props.ariaLabel}
           className="modal"
           overlayClassName="modal-overlay"
         >
           <div className="modal__close">
             <button onClick={this.closeModal} className="close-button" type="button">
-              <span className="visually-hidden">close</span>
+              <span className="visually-hidden">Close modal: {this.props.ariaLabel}</span>
             </button>
           </div>
           <div className="modal__inner">
-            <h2 id="heading_id_placehoder" className="modal__title">Lorem Ipsum</h2>
             <div className="modal__content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                nec tortor nunc. Nunc eget sapien nunc. Suspendisse tincidunt
-                tristique semper. Phasellus imperdiet nunc at vestibulum
-                elementum. Fusce congue suscipit pellentesque. Nunc volutpat
-                sollicitudin gravida. Ut euismod ligula eget mi efficitur,
-                vulputate facilisis purus eleifend. Donec lacinia justo nec
-                lectus ornare, vel maximus justo pretium.
-              </p>
-              <p>ed euismod tempus posuere. Vestibulum consectetur tincidunt
-                nisl tristique eleifend. Aenean sollicitudin, arcu nec
-                tincidunt sodales, augue justo elementum mauris, nec molestie
-                mi felis sed nisi. Vestibulum bibendum tortor a interdum
-                ultrices.
-              </p>
-            </div>
-            <div className="modal__footer">
-              <form>
-                <button>tab navigation</button>
-                <button>stays</button>
-                <button>inside</button>
-                <button>the modal</button>
-              </form>
+              {this.props.modalContent}
             </div>
           </div>
         </Modal>
@@ -76,5 +50,15 @@ class FoiaModal extends Component {
     );
   }
 }
+
+FoiaModal.propTypes = {
+  triggerText: PropTypes.string,
+  ariaLabel: PropTypes.string.isRequired,
+  modalContent: PropTypes.object.isRequired,
+};
+
+FoiaModal.defaultProps = {
+  triggerText: 'Filter Results',
+};
 
 export default FoiaModal;
